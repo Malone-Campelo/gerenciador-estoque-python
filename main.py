9b50ce8f3e80767a3202a535077155d06be0ea36
Malone-Campelo <malonycampelo1@gmail.com>
2024-06-23 12:56:10 -0300

    erro de voltar ao menu corrigida

--- main.py
+++ main.py
@@ -1,84 +1,144 @@
-# Lista
+import os
+from time import sleep
 estoque = [
-    { "produto": "Lapis", "quantidade": 1, "valor": 0.50 },
+    { "produto": "Lapis", "quantidade": 2, "valor": 0.50 },
 ]
-# Função para adicionar, nome produto, quantidade e valor
-def adicionar_produto(estoque):
-    produto = input("Inserir produto: ")
+
+vendas = []
+
+def mostrar_menu():
+    print("""
+[ 1 ] Realizar Venda
+[ 2 ] Adicionar Produto
+[ 3 ] Atualizar Produto
+[ 4 ] Estoque
+[ 5 ] Mostrar Lucro do Dia
+[ 0 ] Sair
+""")
+
+def realizar_venda():
+    while True:
+        produto = input("Nome do produto (ou 'finalizar' para encerrar): ")
+        if produto.lower() == "finalizar":
+            break
+
+        quantidade = int(input("Qtd: "))
+        produto_encontrado = False
+        for item in estoque:
+            if item['produto'].lower() == produto.lower():
+                if item['quantidade'] >= quantidade:
+                    item['quantidade'] -= quantidade
+                    total_venda = quantidade * item['valor']
+                    vendas.append(total_venda)
+                    print(f"Venda realizada: {produto} x {quantidade} = {total_venda:.2f}R$")
+                    produto_encontrado = True
+                else:
+                    print("Quantidade em estoque insuficiente.")
+                break
+
+        if not produto_encontrado:
+            print("Produto não encontrado no estoque.")
+        sleep(2)
+
+def adicionar_produtos():
+    while True:
+        produto = input("Inserir produto (ou 'finalizar' para encerrar): ")
+        if produto.lower() == "finalizar":
+            break
+        quantidade = int(input("Qtd: "))
+        preco = float(input("Valor R$: "))
+
+        # Verificação e confirmação
+        print(f"\nConfirme as informações:")
+        print(f"Produto: {produto}")
+        print(f"Quantidade: {quantidade}")
+        print(f"Valor: {preco:.2f}R$")
+        confirmacao = input("As informações estão corretas? (s/n): ").lower()
+        print("Adicionando produto.....")
+        sleep(2)
+
+        if confirmacao == "s":
+            if quantidade > 0 and preco > 0:
+                estoque.append({"produto": produto, "quantidade": quantidade, "valor": preco})
+                print("Adicionado com sucesso!")
+            else:
+                print("ERRO: quantidade ou preço inválido!")
+        else:
+            print("Operação cancelada.")
+        sleep(2)
+
+def atualizar_produto():
+    produto = input("Nome do produto (ou 'voltar' para cancelar): ")
+    if produto.lower() == 'voltar':
+        return
     quantidade = int(input("Qtd: "))
-    preco = float(input("Valor R$: "))
-    
+    preco = float(input("Preço: "))
+
     # Verificação e confirmação
     print(f"\nConfirme as informações:")
     print(f"Produto: {produto}")
     print(f"Quantidade: {quantidade}")
-    print(f"Valor: R${preco:.2f}")
-    confirmacao = input("As informações estão corretas: s/n:  ").lower()
-   
+    print(f"Valor: {preco:.2f}R$")
+    confirmacao = input("As informações estão corretas? (s/n): ").lower()
+    print("Atualizando....")
+    sleep(2)
+
     if confirmacao == "s":
         if quantidade > 0 and preco > 0:
-            estoque.append({"produto": produto, "quantidade": quantidade, "valor": preco})
-            print("Adicionado com sucesso!")
+            produto_encontrado = False
+            for item in estoque:
+                if item['produto'].lower() == produto.lower():
+                    item['quantidade'] = quantidade
+                    item['valor'] = preco
+                    produto_encontrado = True
+                    print("Atualizado com sucesso!")
+                    break
+            if not produto_encontrado:
+                print("Produto não encontrado no estoque.")
         else:
-            print("ERRO: quantidade inválida! ")
+            print("ERRO, quantidade ou valor incorreto.")
     else:
         print("Operação cancelada.")
-        
-            
-def atualizar_estoque(estoque):
-    produto = input("Qual produto deseja ajustar: ").strip().lower
-    produto_encontrado = False
-    for item in estoque:
-        if item["produto"].lower() == produto:
-            produto_encontrado = True
-            quantidade = int(input("Nova Qtd: "))
-            preco = float(input("Preço atualizado: "))
-            
-     # Verificar e confirmar
-    print(f"\nConfirme as informações:")
-    print(f"Produto: {produto}")
-    print(f"Quantidade: {quantidade}")
-    print(f"Valor: R${preco:.2f}")
-    
-    confirmacao = input("As informações estão corretas? (s/n):  ").lower()
-    if confirmacao == "s":
-        if quantidade >= 0 and preco >= 0:
-            item["quantidade"] = quantidade
-            item["preço"] = preco
-            print("Produto atualizado com sucesso!")
-        else:
-            print("ERRO, Valor inválido.")
-    else:
-        print("Operação cancelada!")
-        return
-print("Produto não encontrado.")
+    sleep(2)
 
-def visualizar_estoque(estoque):
-    print("\nEstoque:")
+def exibir_estoque():
+    print("\nEstoque atual:")
     for item in estoque:
-        print(f"produto: {item['produto']} | quantidade: {item['quantidade']} | valor: {item['valor']:.2f}")
-    print()
+        print(f"Produto: {item['produto']}, Quantidade: {item['quantidade']}, Valor: {item['valor']:.2f}R$")
+    input("\nPressione Enter para voltar ao menu...")
 
-print("{:-^30}".format("Controle de estoque"))
-print("""
-[ 1 ] Adicionar Produto
-[ 2 ] Atualizar Produto
-[ 3 ] Estoque
-[ 4 ] Sair
-""")
+def mostrar_lucro():
+    total_lucro = sum(vendas)
+    print(f"\nLucro total do dia: {total_lucro:.2f}R$")
+    input("\nPressione Enter para voltar ao menu...")
 
 while True:
-    escolha = (input("Selecione o numero: "))
-    
+    # Limpar o terminal
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    mostrar_menu()
+    escolha = input("Escolha a opção: ")
+
     if escolha == '1':
-        adicionar_produto(estoque)
+        realizar_venda()
+
     elif escolha == '2':
-        atualizar_estoque(estoque)
+        adicionar_produtos()
+
     elif escolha == '3':
-        visualizar_estoque(estoque)
+        atualizar_produto()
+
     elif escolha == '4':
-        print("Saindo dos sistema")
+        exibir_estoque()
+
+    elif escolha == '5':
+        mostrar_lucro()
+
+    elif escolha == '0':
+        print("Saindo do sistema...")
+        sleep(1)
         break
+
     else:
         print("Opção inválida. Tente novamente.")
-    
+        sleep(2)
--- sem-função.py
+++ sem-função.py
@@ -1,84 +1,95 @@
 from time import sleep
+
 estoque = [
     { "produto": "Lapis", "quantidade": 2, "valor": 0.50 },
 ]
 
-print("""
-[ 1 ] Realizar Venda
-[ 2 ] Adicionar Produto
-[ 3 ] Atualizar Produto
-[ 4 ] Estoque
-[ 0 ] Sair
-""")
-
 while True:
+    print("""
+    [ 1 ] Realizar Venda
+    [ 2 ] Adicionar Produto
+    [ 3 ] Atualizar Produto
+    [ 4 ] Estoque
+    [ 0 ] Sair
+    """)
     escolha = input("Escolha a opção: ")
-    if escolha == '2':
-        produto = input("Inserir produto: ")
-        quantidade = int(input("Qtd: "))
-        preco = float(input("Valor R$: "))
+    
+    if escolha == '1':
+        print('')
+        # ainda falta fazer a parte das vendas
+        
+        
+    elif escolha == '2':
+        while True:
+            produto = input("Nome do produto (ou 'voltar' para encerrar): ")
+            if produto.lower() == 'voltar':
+                break
+            quantidade = int(input('Qtd: '))
+            preco = float(input('Valor R$: '))
 
-        # Verificação e confirmação
-        print(f"\nConfirme as informações:")
-        print(f"Produto: {produto}")
-        print(f"Quantidade: {quantidade}")
-        print(f"Valor: {preco:.2f}R$")
-        confirmacao = input("As informações estão corretas? (s/n): ").lower()
-        print("Adicionando produto.....")
-        sleep(2)
+            # Verificação e confirmação
+            print(f'Produto: {produto}')
+            print(f'Quantidade: {quantidade}')
+            print(f'Valor: {preco:.2f}R$')
+            confirmacao = input('As informações estão corretas? (s/n): ').lower()
+            print('Aguarde...')
+            sleep(2)
 
-        if confirmacao == "s":
-            if quantidade > 0 and preco > 0:
-                estoque.append({"produto": produto, "quantidade": quantidade, "valor": preco})
-                print("Adicionado com sucesso!")
+            if confirmacao == 's':
+                if quantidade > 0 and preco > 0:
+                    estoque.append({'produto': produto, 'quantidade': quantidade, 'valor': preco})
+                    print('Adicionado com sucesso!')
+                else:
+                    print('ERRO: quantidade ou preço inválido!')
             else:
-                print("ERRO: quantidade ou preço inválido!")
-        else:
-            print("Operação cancelada.")
+                print('Operação cancelada.')
+            break
     
     elif escolha == '3':
-        # Atualizar produto
-        produto = input("Nome do produto (ou 'voltar' para cancelar): ")
-        if produto.lower() == 'voltar':
-            continue
-        quantidade = int(input("Qtd: "))
-        preco = float(input("Preço: "))
+        while True:
+            produto = input("Nome do produto (ou 'voltar' para cancelar): ")
+            if produto.lower() == 'voltar':
+                break
+            quantidade = int(input('Qtd: '))
+            preco = float(input('Preço: '))
 
-        # Verificação e confirmação
-        print(f"\nConfirme as informações:")
-        print(f"Produto: {produto}")
-        print(f"Quantidade: {quantidade}")
-        print(f"Valor: {preco:.2f}R$")
-        confirmacao = input("As informações estão corretas? (s/n): ").lower()
-        print("Atualizando....")
-        sleep(2)
+            # Verificação e confirmação
+            print(f'Produto: {produto}')
+            print(f'Quantidade: {quantidade}')
+            print(f'Valor: {preco:.2f}R$')
+            confirmacao = input('As informações estão corretas? (s/n): ').lower()
+            print('Aguarde...')
+            sleep(2)
 
-        if confirmacao == "s":
-            if quantidade > 0 and preco > 0:
-                produto_encontrado = False
-                for item in estoque:
-                    if item['produto'].lower() == produto.lower():
-                        item['quantidade'] = quantidade
-                        item['valor'] = preco
-                        produto_encontrado = True
-                        print("Atualizado com sucesso!")
-                        break
-                if not produto_encontrado:
-                    print("Produto não encontrado no estoque.")
+            if confirmacao == "s":
+                if quantidade == 0 and preco == 0:
+                    produto_encontrado = False
+                    for item in estoque:
+                        if item['produto'].lower() == produto.lower():
+                            item['quantidade'] = quantidade
+                            item['valor'] = preco
+                            produto_encontrado = True
+                            print('Atualizado com sucesso!')
+                            break
+                    if not produto_encontrado:
+                        print('Produto não encontrado no estoque.')
+                else:
+                    print('ERRO, quantidade ou valor incorreto.')
             else:
-                print("ERRO, quantidade ou valor incorreto.")
-        else:
-            print("Operação cancelada.")
+                print('Operação cancelada.')
+            break
     
     elif escolha == '4':
         # Exibir estoque
-        print("\nEstoque atual:")
+        print('\nEstoque atual:')
         for item in estoque:
-            print(f"Produto: {item['produto']}, Quantidade: {item['quantidade']}, Valor: {item['valor']:.2f}R$")
+            print(f"Produto: {item['produto']} | Quantidade: {item['quantidade']} | Valor: {item['valor']:.2f}R$")
+        input('\nPrecione Enter para voltar ao menu...')
     
     elif escolha == '0':
-        print("Saindo do sistema")
+        print('Saindo do sistema...')
+        sleep(1)
         break
     
     else:
-        print("Opção inválida. Tente novamente.")
+        print('Opção inválida. Tente novamente.')
