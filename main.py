cdec11227aa7bcc3c879f489361184d87cbe56ed
Malone-Campelo <malonycampelo1@gmail.com>
2024-06-24 22:02:56 -0300

    Concluido

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 from time import sleep
+
 estoque = [
-    { "produto": "Lapis", "quantidade": 2, "valor": 0.50 },
+    { "produto": "Cuscuz", "quantidade": 2, "valor": 0.99 },
+    { "produto": "Arroz", "quantidade": 2, "valor": 25.00 },
+    { "produto": "Açucar", "quantidade": 5, "valor": 3.99 }
 ]
 
 vendas = []
@@ -18,16 +21,15 @@ def mostrar_menu():
 
 def realizar_venda():
     while True:
-        produto = input("Nome do produto (ou 'finalizar' para encerrar): ")
-        if produto.lower() == "finalizar":
+        produto = input("Nome do produto (ou 'Voltar' para encerrar): ")
+        if produto.lower() == "voltar":
             break
-
         quantidade = int(input("Qtd: "))
         produto_encontrado = False
         for item in estoque:
-            if item['produto'].lower() == produto.lower():
-                if item['quantidade'] >= quantidade:
-                    item['quantidade'] -= quantidade
+            if item["produto"].strip().lower() == produto:
+                if item["quantidade"] >= quantidade:
+                    item["quantidade"] -= quantidade
                     total_venda = quantidade * item['valor']
                     vendas.append(total_venda)
                     print(f"Venda realizada: {produto} x {quantidade} = {total_venda:.2f}R$")
@@ -35,32 +37,28 @@ def realizar_venda():
                 else:
                     print("Quantidade em estoque insuficiente.")
                 break
-
         if not produto_encontrado:
             print("Produto não encontrado no estoque.")
         sleep(2)
 
 def adicionar_produtos():
     while True:
-        produto = input("Inserir produto (ou 'finalizar' para encerrar): ")
-        if produto.lower() == "finalizar":
+        produto = input("Inserir produto (ou 'voltar' para encerrar): ")
+        if produto.lower() == "voltar":
             break
         quantidade = int(input("Qtd: "))
         preco = float(input("Valor R$: "))
-
-        # Verificação e confirmação
-        print(f"\nConfirme as informações:")
+        print("--" * 30)
         print(f"Produto: {produto}")
         print(f"Quantidade: {quantidade}")
         print(f"Valor: {preco:.2f}R$")
         confirmacao = input("As informações estão corretas? (s/n): ").lower()
-        print("Adicionando produto.....")
+        print("Aguarde...")
         sleep(2)
-
         if confirmacao == "s":
             if quantidade > 0 and preco > 0:
                 estoque.append({"produto": produto, "quantidade": quantidade, "valor": preco})
-                print("Adicionado com sucesso!")
+                print("Produto adicionado com sucesso!")
             else:
                 print("ERRO: quantidade ou preço inválido!")
         else:
@@ -73,18 +71,14 @@ def atualizar_produto():
         return
     quantidade = int(input("Qtd: "))
     preco = float(input("Preço: "))
-
-    # Verificação e confirmação
-    print(f"\nConfirme as informações:")
     print(f"Produto: {produto}")
     print(f"Quantidade: {quantidade}")
     print(f"Valor: {preco:.2f}R$")
     confirmacao = input("As informações estão corretas? (s/n): ").lower()
     print("Atualizando....")
     sleep(2)
-
     if confirmacao == "s":
-        if quantidade > 0 and preco > 0:
+        if quantidade >= 0 and preco >= 0:
             produto_encontrado = False
             for item in estoque:
                 if item['produto'].lower() == produto.lower():
@@ -104,7 +98,7 @@ def atualizar_produto():
 def exibir_estoque():
     print("\nEstoque atual:")
     for item in estoque:
-        print(f"Produto: {item['produto']}, Quantidade: {item['quantidade']}, Valor: {item['valor']:.2f}R$")
+        print(f"Produto: {item['produto']} | Quantidade: {item['quantidade']} | Valor: {item['valor']:.2f} R$")
     input("\nPressione Enter para voltar ao menu...")
 
 def mostrar_lucro():
@@ -113,32 +107,24 @@ def mostrar_lucro():
     input("\nPressione Enter para voltar ao menu...")
 
 while True:
-    # Limpar o terminal
     os.system('cls' if os.name == 'nt' else 'clear')
 
     mostrar_menu()
     escolha = input("Escolha a opção: ")
-
     if escolha == '1':
         realizar_venda()
-
     elif escolha == '2':
         adicionar_produtos()
-
     elif escolha == '3':
         atualizar_produto()
-
     elif escolha == '4':
         exibir_estoque()
-
     elif escolha == '5':
         mostrar_lucro()
-
     elif escolha == '0':
         print("Saindo do sistema...")
         sleep(1)
         break
-
     else:
         print("Opção inválida. Tente novamente.")
         sleep(2)
