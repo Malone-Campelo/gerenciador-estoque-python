3aca1e6ab2d9941df56b0ae6b041421bf4bddcd6
Malone-Campelo <malonycampelo1@gmail.com>
2024-07-02 19:28:24 -0300

    correção de erros, finalizado!

--- main.py
+++ main.py
@@ -24,29 +24,51 @@ def realizar_venda():
         produto = input("Nome do produto (ou 'Voltar' para encerrar): ")
         if produto.lower() == "voltar":
             break
-        quantidade = int(input("Qtd: "))
-        produto_encontrado = False
-        for item in estoque:
-            if item["produto"].strip().lower() == produto:
-                if item["quantidade"] >= quantidade:
-                    item["quantidade"] -= quantidade
-                    total_venda = quantidade * item['valor']
-                    vendas.append(total_venda)
-                    print(f"Venda realizada: {produto} x {quantidade} = {total_venda:.2f}R$")
-                    produto_encontrado = True
-                else:
-                    print("Quantidade em estoque insuficiente.")
-                break
-        if not produto_encontrado:
-            print("Produto não encontrado no estoque.")
-        sleep(2)
+        
+        while True:
+            try:
+                quantidade = int(input("Qtd: "))
+                if quantidade <= 0:
+                    raise ValueError("A quantidade deve ser um número inteiro positivo.")
+            except ValueError:
+                print("ERRO: A quantidade deve ser um número inteiro positivo. Tente novamente.")
+                continue
+
+        
 
 def adicionar_produtos():
     while True:
         produto = input("Inserir produto (ou 'voltar' para encerrar): ")
         if produto.lower() == "voltar":
             break
-        quantidade = int(input("Qtd: "))
+
+        try:
+            quantidade = int(input("Qtd: "))
+            if quantidade <= 0:
+                raise ValueError ("ERRO: Quantidade so é permitido número. Tente novamente.")
+        except ValueError:
+            print("ERRO: A quantidade deve ser um número inteiro positivo. Tente novamente.")
+            continue
+
+        preco = float(input("Valor R$: "))
+        print("--" * 30)
+        print(f"Produto: {produto}")
+        print(f"Quantidade: {quantidade}")
+        print(f"Valor: {preco:.2f}R$")
+        confirmacao = input("As informações estão corretas? (s/n): ").lower()
+        print("Aguarde...")
+        sleep(2)
+
+        if confirmacao == "s":
+            if quantidade > 0 and preco > 0:
+                estoque.append({"produto": produto, "quantidade": quantidade, "valor": preco})
+                print("Produto adicionado com sucesso!")
+            else:
+                print("ERRO: quantidade ou preço inválido!")
+        else:
+            print("Operação cancelada.")
+        sleep(2)    
+        
         preco = float(input("Valor R$: "))
         print("--" * 30)
         print(f"Produto: {produto}")
@@ -66,34 +88,43 @@ def adicionar_produtos():
         sleep(2)
 
 def atualizar_produto():
-    produto = input("Nome do produto (ou 'voltar' para cancelar): ")
-    if produto.lower() == 'voltar':
-        return
-    quantidade = int(input("Qtd: "))
-    preco = float(input("Preço: "))
-    print(f"Produto: {produto}")
-    print(f"Quantidade: {quantidade}")
-    print(f"Valor: {preco:.2f}R$")
-    confirmacao = input("As informações estão corretas? (s/n): ").lower()
-    print("Atualizando....")
-    sleep(2)
-    if confirmacao == "s":
-        if quantidade >= 0 and preco >= 0:
-            produto_encontrado = False
-            for item in estoque:
-                if item['produto'].lower() == produto.lower():
-                    item['quantidade'] = quantidade
-                    item['valor'] = preco
-                    produto_encontrado = True
-                    print("Atualizado com sucesso!")
-                    break
-            if not produto_encontrado:
-                print("Produto não encontrado no estoque.")
+    while True:    
+        produto = input("Nome do produto (ou 'voltar' para cancelar): ").strip()
+        if produto.lower() == 'voltar':
+            return
+    
+        try:
+            quantidade = int(input("Qtd: "))
+            if quantidade >= 0:
+                raise ValueError ("ERRO: Quantidade so é permitido número. Tente novamente.")
+        except ValueError:
+            print("ERRO: A quantidade deve ser um número inteiro positivo. Tente novamente.")
+            continue
+      
+        preco = float(input("Preço: "))
+        print(f"Produto: {produto}")
+        print(f"Quantidade: {quantidade}")
+        print(f"Valor: {preco:.2f}R$")
+        confirmacao = input("As informações estão corretas? (s/n): ").lower()
+        print("Atualizando....")
+        sleep(2)
+        if confirmacao == "s":
+            if quantidade >= 0 and preco >= 0:
+                produto_encontrado = False
+                for item in estoque:
+                    if item['produto'].lower() == produto.lower():
+                        item['quantidade'] = quantidade
+                        item['valor'] = preco
+                        produto_encontrado = True
+                        print("Atualizado com sucesso!")
+                        break
+                if not produto_encontrado:
+                    print("Produto não encontrado no estoque.")
+            else:
+                print("ERRO, quantidade ou valor incorreto.")
         else:
-            print("ERRO, quantidade ou valor incorreto.")
-    else:
-        print("Operação cancelada.")
-    sleep(2)
+            print("Operação cancelada.")
+        sleep(2)
 
 def exibir_estoque():
     print("\nEstoque atual:")
